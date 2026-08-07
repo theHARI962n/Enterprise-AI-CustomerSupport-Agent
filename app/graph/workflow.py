c841a5fce24cfaf01d3427453a6643dcbfa4411f
theHARI962n <rajul1874@gmail.com>
2026-08-07 23:44:47 +0530

    update workflow for graph

--- app/graph/workflow.py
+++ app/graph/workflow.py
@@ -5,19 +5,21 @@ from app.graph.state import SupportState
 
 # Import your agent node function
 from app.agents.intent_agent import intent_agent
-
+from app.agents.knowledge_agent import knowledge_agent
 
 # Step 2: Create the Builder (deciding that all stations belong to SupportState)
 builder = StateGraph(SupportState)
 
 # Step 3: Register Nodes (mapping node names to Python functions)
 builder.add_node("intent", intent_agent)
+builder.add_node("knowledge", knowledge_agent)
 
 # Step 4: Connect START to the first node
 builder.add_edge(START, "intent")
 
 # Step 5: Connect the node to END
-builder.add_edge("intent", END)
+builder.add_edge("intent", "knowledge")
+builder.add_edge("knowledge", END)
 
 # Step 6: Compile the graph into an executable application
 graph = builder.compile()
